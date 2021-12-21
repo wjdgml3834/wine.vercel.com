dc7a914d74abc3a8c04bb86dc435c883b292c0e6
Jung Hee <wjdgml3834@naver.com>
2021-12-21 22:50:30 +0900

    api 연동

---
+++ src/hooks/useWineData.ts
@@ -0,0 +1,8 @@
+import useSWR from "swr";
+import { fetcher } from "../utils/fetcher";
+import { WINE_API_ENDPOINT } from "../constants";
+
+export const useWineData = (path: string) => {
+  return useSWR(`${WINE_API_ENDPOINT}${path}`, fetcher);
+};
+// port를 파라미터로 받아야한다
--- src/pages/wines/port.tsx
+++ src/pages/wines/port.tsx
@@ -1,10 +1,30 @@
 import type { NextPage } from "next";
+import { useWineData } from "../../hooks/useWineData";
+import { Wine } from "../../types/Wine";
 
 // 타입 선언 :NextPage -> 타입스크립트에서 나옴
 const Port: NextPage = () => {
+  const { data, error } = useWineData("whites");
+
+  // 에러가 나면 에러 해주고 데이터 잇으면 데이터 뱉어줌
+  if (error) return <div>failed to Loading..</div>;
+  if (!data) return <div>Loading</div>;
+  // 에러 낫을때랑. 로딩일때 처리
   return (
     <div>
       <h1> Wines Port</h1>
+      <main>
+        {data.map((wineData: Wine) => {
+          // types에서 불러옴
+          const { id, wine, winery } = wineData;
+          return (
+            <div key={`port-wine-list-${wineData.id}`}>
+              <h1>{wineData.wine}</h1>
+              <p>{wineData.winery}</p>
+            </div>
+          );
+        })}
+      </main>
     </div>
   );
 };
